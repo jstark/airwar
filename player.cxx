614ae7f0659e1b70bba908af9902d2bbaf404e4f
Ioannis Fourkiotis <jsstark@gmail.com>
2013-09-28 21:20:51 +0300

    basic key bindings for player's aircraft

--- player.cxx
+++ player.cxx
@@ -14,6 +14,7 @@ using airwar::CommandQueue;
 using airwar::Category;
 using airwar::SceneNode;
 using airwar::Aircraft;
+using airwar::PlayerAction;
 
 namespace
 {
@@ -33,39 +34,87 @@ namespace
 
 		void operator()(Aircraft& node, sf::Time dt) const
 		{
-			sf::Vector2f cvel = node.velocity();
-			node.set_velocity(cvel + velocity);
+			node.accelerate(velocity);
 		}
 
 		sf::Vector2f velocity;
 	};
 }
 
+Player::Player()
+{
+	// default key bindings
+	key_bindings[sf::Keyboard::Left] = PlayerAction::MoveLeft;
+	key_bindings[sf::Keyboard::Right] = PlayerAction::MoveRight;
+	key_bindings[sf::Keyboard::Up] = PlayerAction::MoveUp;
+	key_bindings[sf::Keyboard::Down] = PlayerAction::MoveDown;
+
+	// initial action bindings
+	initialize_actions();
+
+	// assign categories
+	for (auto & p : action_bindings)
+	{
+		p.second.category = static_cast<unsigned int>(Category::PlayerAircraft);
+	}
+}
+
+void Player::initialize_actions()
+{
+	const float player_speed = 200.f;
+	action_bindings[PlayerAction::MoveLeft].action = derived_action<Aircraft>(MoveAircraft(-player_speed, 0.0f));
+	action_bindings[PlayerAction::MoveRight].action = derived_action<Aircraft>(MoveAircraft(+player_speed, 0.0f));
+	action_bindings[PlayerAction::MoveUp].action = derived_action<Aircraft>(MoveAircraft(0.0f, -player_speed));
+	action_bindings[PlayerAction::MoveDown].action = derived_action<Aircraft>(MoveAircraft(0.0f, +player_speed));
+}
+
 // an example of a one-time command, we output the position of the player
 // when he presses 'P'
 void Player::handle_event(const sf::Event& event, CommandQueue& commands)
 {
-	if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
+	if (event.type == sf::Event::KeyPressed)
 	{
-		Command output;
-		output.category = static_cast<unsigned int>(Category::PlayerAircraft);
-		output.action = [](SceneNode& s, sf::Time dt)
+		auto found = key_bindings.find(event.key.code);
+		if (found != key_bindings.end() && !is_realtime_action(found->second))
 		{
-			std::cout << s.getPosition().x << ", " << s.getPosition().y << "\n";
-		};
-		commands.push(output);
+			commands.push(action_bindings[found->second]);
+		}
 	}
 }
 
 void Player::handle_realtime_input(CommandQueue& commands)
 {
-	const float player_speed{ 30.f };
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	for (const auto& p : key_bindings)
 	{
-		Command move_left;
-		move_left.category = static_cast<unsigned int>(Category::PlayerAircraft);
-		move_left.action = derived_action<Aircraft>(MoveAircraft(-player_speed, 0.f));
-		commands.push(move_left);
+		if (sf::Keyboard::isKeyPressed(p.first) && is_realtime_action(p.second))
+		{
+			commands.push(action_bindings[p.second]);
+		}
+	}
+}
+
+sf::Keyboard::Key Player::assigned_key(PlayerAction action) const
+{
+	for (const auto& p : key_bindings)
+	{
+		if (p.second == action)
+		{
+			return p.first;
+		}
+	}
+	return sf::Keyboard::Unknown;
+}
+
+bool Player::is_realtime_action(PlayerAction action)
+{
+	switch (action)
+	{
+	case PlayerAction::MoveLeft:
+	case PlayerAction::MoveRight:
+	case PlayerAction::MoveDown:
+	case PlayerAction::MoveUp:
+		return true;
+	default:
+		return false;
 	}
 }
--- player.hxx
+++ player.hxx
@@ -1,20 +1,36 @@
 #ifndef AIRWAR_PLAYER_HXX_INCLUDED
 #define AIRWAR_PLAYER_HXX_INCLUDED
 
-namespace sf
-{
-	class Event;
-}
+#include "SFML/Window.hpp"
+#include <map>
 
 namespace airwar
 {
+	struct Command;
 	class CommandQueue;
+
+	enum class PlayerAction
+	{
+		MoveLeft,
+		MoveRight,
+		MoveUp,
+		MoveDown,
+		ActionCount
+	};
 
 	class Player
 	{
 	public:
+		Player();
+		sf::Keyboard::Key assigned_key(PlayerAction action) const;
 		void handle_event(const sf::Event &event, CommandQueue &commands);
 		void handle_realtime_input(CommandQueue& commands);
+	private:
+		void initialize_actions();
+		static bool is_realtime_action(PlayerAction action);
+
+		std::map<sf::Keyboard::Key, PlayerAction> key_bindings;
+		std::map<PlayerAction, Command> action_bindings;
 	};
 }
 
